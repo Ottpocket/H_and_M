de893d56e4e21e54573372fe2c802ccb7e1ab259
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2022-04-14 07:23:58 -0500

    Update backbone.py

--- scripts/backbone.py
+++ scripts/backbone.py
@@ -1,3 +1,4 @@
+#https://www.kaggle.com/code/ottpocket/nn-starter-5
 PROJECT_NAME =  #string
 hyperparams = #dict
 1/0 #delete this line once you have installed the kaggle secret code
@@ -5,6 +6,7 @@ hyperparams = #dict
 #imports
 import pandas as pd
 import numpy as np
+import gc
 
 
 #Setting up WandB
@@ -19,11 +21,48 @@ wandb.init(project= PROJECT_NAME, config=hyperparams)
 config = wandb.config
 
 #upload data
+ss = pd.read_parquet('../input/hm-parquet/sample_submission')
+articles = pd.read_parquet('../input/hm-parquet/articles')
+train = pd.read_parquet('/kaggle/input/hm-parquet/transactions')
+customers = pd.read_parquet('../input/hm-parquet/customers')
+train['article_id'] = train.article_id.astype('int32')
+train.t_dat = pd.to_datetime(train.t_dat)
 
-for i in range(3)
+#3 fold val dict with the test
+val_dict = {'val_0': {'train': train.t_dat < pd.to_datetime('2020-09-16'),
+                'val': train.t_dat >= pd.to_datetime('2020-09-16')},
+            'val_1': {'train': train.t_dat < pd.to_datetime('2020-09-09'),
+                'val': (train.t_dat >= pd.to_datetime('2020-09-09')) & (train.t_dat < pd.to_datetime('2020-09-16'))}, 
+            'val_2': {'train': train.t_dat < pd.to_datetime('2020-09-02'),
+                'val': (train.t_dat >= pd.to_datetime('2020-09-02')) & (train.t_dat < pd.to_datetime('2020-09-09'))}
+            'test': {}}
+
+
+#############################
+#Val block
+#############################
+for key in val_dict.keys():
+    print(f'Beginning {key}')
+    if key != 'test':    
+        train_df = train[val_dict[key]['train']].copy().reset_index(drop=True)
+        val = train[val_dict[key]['val']].reset_index(drop=True)
+        val = valid.rename({'article_id':'prediction'},axis=1)
+        val['true'] =\
+            val.prediction.apply(lambda x: ' '.join(['0'+str(k) for k in x]))
+    else:
+        train_df = train.copy()
+        del train; gc.collect()
+        
     #transformations DONE BY FUNCTIONS
-    #get scores
-    #save scores to WandB
     
-#Get test values 
-#save test score
+    
+    #get scores
+    if key != 'test':
+        score = mapk(
+                        val['true'].map(lambda x: x.split()), 
+                        train_df['prediction'].map(lambda x: x.split()), 
+                        k=12
+                    )
+        wandb.run.summary[key] = score
+    else:
+        train_df.to_csv('sub.csv',index=False)
