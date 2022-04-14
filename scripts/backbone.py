a5693a05742c804db2c62bf9b9ce913a5fe45230
El Conde de Ñáñaño <64142335+Ottpocket@users.noreply.github.com>
2022-04-14 06:26:21 -0500

    Update backbone.py

--- scripts/backbone.py
+++ scripts/backbone.py
@@ -1,4 +1,22 @@
+PROJECT_NAME =  #string
+hyperparams = #dict
+1/0 #delete this line once you have installed the kaggle secret code
+
 #imports
+import pandas as pd
+import numpy as np
+
+
+#Setting up WandB
+import wandb
+from kaggle_secrets import UserSecretsClient
+user_secrets = UserSecretsClient()
+secret_value_0 = user_secrets.get_secret("Api_Key")
+!wandb login $secret_value_0
+
+#See https://docs.wandb.com/library/init for more details
+wandb.init(project= PROJECT_NAME, config=hyperparams)
+config = wandb.config
 
 #upload data
 
